b4c2bff45a9072894b71c90d895a838bde754f21
Kolonskaia Anastasiia <anagrimonia@gmail.com>
2021-03-28 12:20:05 +0300

    Fixed theme class changing

--- src/application/view.ts
+++ src/application/view.ts
@@ -38,5 +38,6 @@ export const initProgress = (parent: HTMLDivElement) => {
 }
 
 export const setElementTheme = (elem: HTMLElement, theme: SlideTheme) => {
+    elem.classList.remove(`theme_${theme ==='light' ? 'dark' : 'light'}`);
     elem.classList.add(`theme_${theme}`);
 }
